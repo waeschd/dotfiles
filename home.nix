0d3a0e19daf239b04d381474b8376408f9c9c79d
Sebastian Russer <sebastian.russer@fau.de>
2026-08-10 11:57:38 +0200

    add home.nix

---
+++ home.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }
+
+{
+  home.stateVersion = "26.05";
+  home.username = "seru";
+  home.homeDirectory = "/home/seru";
+  programs.git.enable = true;
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo I use nixos";
+    };
+  };
+}
