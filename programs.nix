7ea43aa4281ab8fe6dd55b2205715e0701a78c06
Sebastian Russer <sebastian.russer@fau.de>
2026-08-10 11:38:39 +0200

    more

--- programs.nix
+++ programs.nix
@@ -23,7 +23,7 @@
     neovim
     git
     curl
-    adwaita-icon-theme
+    papirus-icon-theme
   ];
 
   # My Traceroute, combines the functionality of the 'ping' and 'traceroute' into a single interface.
