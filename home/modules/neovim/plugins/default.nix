d4d52d02bba99e45a02048dae6d0e89114e3f2a6
Sebastian Russer <sebastian.russer@fau.de>
2026-09-20 13:04:18 +0200

    more

--- home/modules/neovim/plugins/default.nix
+++ home/modules/neovim/plugins/default.nix
@@ -1,5 +1,10 @@
 { pkgs, lib, ... }:
 [
+  {
+    plugin = pkgs.vimPlugins.which-key-nvim;
+    type = "lua";
+    config = builtins.readFile ./which-key.lua;
+  }
 ]
 ++ (import ./colorscheme/colorscheme.nix { inherit pkgs; })
 ++ (import ./lsp/lsp.nix { inherit pkgs lib; })
