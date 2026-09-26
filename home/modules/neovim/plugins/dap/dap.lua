3b7865601cc95b8e09e0190f9e99186f0a4333e2
Sebastian Russer <sebastian.russer@fau.de>
2026-09-26 12:53:48 +0200

    more

--- home/modules/neovim/plugins/dap/dap.lua
+++ home/modules/neovim/plugins/dap/dap.lua
@@ -258,17 +258,16 @@ vim.keymap.set("n", "<S-Up>", function()
 end, { desc = "(Debug) Down one frame", silent = true })
 
 -- Highlights and Icons
-vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapBreakpoint" })
-vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DapBreakpoint" })
-vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DapBreakpoint" })
-vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DapLogPoint" })
-vim.fn.sign_define("DapStopped", { text = "", texthl = "@variable.parameter.vimdoc", linehl = "DapStoppedBg" })
-
 local function set_dap_highlights()
-  local stopped_bg = vim.o.background == "light" and "#fdf1c4" or "#604918"
-  vim.api.nvim_set_hl(0, "DapStoppedBg", { bg = stopped_bg })
+  vim.api.nvim_set_hl(0, "DapStoppedBg", { link = "NvimDapVirtualTextChanged" })
   vim.api.nvim_set_hl(0, "DapBreakpoint", { link = "DiagnosticError" })
   vim.api.nvim_set_hl(0, "DapLogPoint", { link = "DiagnosticInfo" })
+
+  vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapBreakpoint" })
+  vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DapBreakpoint" })
+  vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DapBreakpoint" })
+  vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DapLogPoint" })
+  vim.fn.sign_define("DapStopped", { text = "", texthl = "@variable.parameter.vimdoc", linehl = "DapStoppedBg" })
 end
 
 set_dap_highlights()
